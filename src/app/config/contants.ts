0a8ffa37c03b6e549e89c99f82c4f681369a1f42
Yokub2144 <suwat.paomong@gmail.com>
2026-07-31 11:16:47 +0700

    Restore local API endpoint configuration in Constants class

--- src/app/config/contants.ts
+++ src/app/config/contants.ts
@@ -4,9 +4,9 @@ import { Injectable } from '@angular/core';
   providedIn: 'root',
 })
 export class Constants {
-  // public readonly API_ENDPOINT: string = 'http://localhost:5277/api';
-  // public readonly URL_signalR: string = 'http://localhost:5277/';
-  public readonly API_ENDPOINT: string =
-    'https://buffetapi-853684840693.asia-southeast3.run.app/api';
-  public readonly URL_signalR: string = 'https://buffetapi-853684840693.asia-southeast3.run.app/';
+  public readonly API_ENDPOINT: string = 'http://localhost:5277/api';
+  public readonly URL_signalR: string = 'http://localhost:5277/';
+  // public readonly API_ENDPOINT: string =
+  //   'https://buffetapi-853684840693.asia-southeast3.run.app/api';
+  // public readonly URL_signalR: string = 'https://buffetapi-853684840693.asia-southeast3.run.app/';
 }
