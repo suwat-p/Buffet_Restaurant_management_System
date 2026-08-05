acf36abadef7885e756e91c0fb36a84cdfcd63ea
Yokub2144 <suwat.paomong@gmail.com>
2026-08-05 20:08:15 +0700

    fix: update API endpoint and SignalR URL to correct production values

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiEndpoint: 'https://buffetapi-853684840693.asia-southeast3.run.app/api',
-  urlSignalR: 'https://buffetapi-853684840693.asia-southeast3.run.app/'
+  apiEndpoint: 'https://buffet-restaurant-api.onrender.com/api',
+  urlSignalR: 'https://buffet-restaurant-api.onrender.com/'
 };
